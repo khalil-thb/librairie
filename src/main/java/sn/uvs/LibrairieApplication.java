1efcce69550b9f91e6244877ba2c87c603fb0a44
khalil-thb <khalilthb09@gmail.com>
2021-07-29 20:25:57 +0000

    premier commit

---
+++ src/main/java/sn/uvs/LibrairieApplication.java
@@ -0,0 +1,13 @@
+package sn.uvs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrairieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrairieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/uvs/LibrairieApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.uvs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrairieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
